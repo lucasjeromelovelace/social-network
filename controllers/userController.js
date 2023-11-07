dc1410695e28d60c700c56143d32e5159400bc88
Lucas Jerome Lovelace <lucasjeromelovelace@Lucass-Air.attlocal.net>
2023-11-07 15:58:04 -0600

    updated  routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { User, Course } = require('../models');
+const { User, Thought } = require('../models');
 
 
 module.exports = {
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -15,9 +15,9 @@ router.route('/').get(getUsers).post(createUser);
 router.route('/:userId').get(getSingleUser).delete(deleteUser);
 
 // /api/users/:userId/thoughts
-router.route('/:userId/thoughts').post(addThought);
+// router.route('/:userId/thoughts').post(addThought);
 
-// /api/users/:userId/thoughts/:thoughtId
-router.route('/:userId/thoughts/:thoughtId').delete(removeThought);
+// // /api/users/:userId/thoughts/:thoughtId
+// router.route('/:userId/thoughts/:thoughtId').delete(removeThought);
 
 module.exports = router;
